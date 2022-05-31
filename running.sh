b567744815b64694551550bcdd96f1d0243b4c49
snoop2head <young_ahn@yonsei.ac.kr>
2022-06-01 08:40:51 +0900

    update trainer according to black formatter

--- running.sh
+++ running.sh
@@ -2,22 +2,22 @@
 python train.py \
 --do_train \
 --fp16 \
---PLM microsoft/codebert-base \
+--PLM microsoft/graphcodebert-base \
 --model_category codebert \
 --model_name RobertaRBERT \
 --logging_strategy steps \
---logging_steps 500 \
+--logging_steps 1000 \
 --evaluation_strategy steps \
 --save_strategy steps \
 --eval_steps 1000 \
 --save_steps 1000 \
 --overwrite_output_dir \
---save_total_limit 5 \
+--save_total_limit 10 \
 --output_dir ./exps \
 --logging_dir ./logs \
 --num_train_epochs 2 \
 --learning_rate 2e-5 \
---per_device_train_batch_size 16 \
+--per_device_train_batch_size 24 \
 --per_device_eval_batch_size 32 \
 --gradient_accumulation_steps 2 \
 --load_best_model_at_end \
@@ -30,7 +30,7 @@ python inference.py \
 --model_category codebert \
 --model_name RobertaRBERT \
 --output_dir results \
---file_name codebert_rbert_EP:2_BS:32_WR:0.05_WD:1e-2_LR:2e-5.csv \
---PLM checkpoints \
+--file_name graphcodebert_rbert_EP:2_BS:32_WR:0.05_WD:1e-2_LR:2e-5.csv \
+--PLM  /home/ubuntu/CodeSimilarity/exps/checkpoint-12000 \
 --fp16 \
 --per_device_eval_batch_size 32
--- train.py
+++ train.py
@@ -12,11 +12,7 @@ from utils.encoder import Encoder
 from utils.collator import DataCollatorWithPadding
 from utils.preprocessor import AnnotationPreprocessor, FunctionPreprocessor
 from trainer import Trainer
-from arguments import (ModelArguments, 
-    DataTrainingArguments, 
-    MyTrainingArguments, 
-    LoggingArguments
-)
+from arguments import ModelArguments, DataTrainingArguments, MyTrainingArguments, LoggingArguments
 
 from transformers import (
     AutoConfig,
@@ -24,6 +20,7 @@ from transformers import (
     AutoModelForSequenceClassification,
     HfArgumentParser,
 )
+
 
 def main():
     parser = HfArgumentParser(
@@ -33,13 +30,17 @@ def main():
     seed_everything(training_args.seed)
 
     load_dotenv(dotenv_path=logging_args.dotenv_path)
-    POOLC_AUTH_KEY = os.getenv("POOLC_AUTH_KEY")  
+    POOLC_AUTH_KEY = os.getenv("POOLC_AUTH_KEY")
 
     # -- Loading datasets
     # dset = load_dataset("PoolC/clone-det-base", use_auth_token=POOLC_AUTH_KEY)
-    dset = load_dataset("PoolC/clone-det-all", use_auth_token=POOLC_AUTH_KEY)
-    random_numbers_train = np.random.randint(0, 6961241, int(500000)) # can be set as a parameter if K-Fold is used
-    random_numbers_val = np.random.randint(0, 6965880, int(50000)) # can be set as a parameter if K-Fold is used
+    dset = load_dataset("PoolC/clone-det-all", use_auth_token=True)
+    random_numbers_train = np.random.randint(
+        0, 6961241, int(1000000)
+    )  # can be set as a parameter if K-Fold is used
+    random_numbers_val = np.random.randint(
+        0, 6965880, int(100000)
+    )  # can be set as a parameter if K-Fold is used
     dset["train"] = dset["train"].select(random_numbers_train)
     dset["val"] = dset["val"].select(random_numbers_val)
     print(dset)
@@ -47,10 +48,11 @@ def main():
     CPU_COUNT = multiprocessing.cpu_count() // 2
 
     MAX_LENGTH = 4000
-    def filter_fn(data) :
-        if len(data['code1']) >= MAX_LENGTH or len(data['code2']) >= MAX_LENGTH :
+
+    def filter_fn(data):
+        if len(data["code1"]) >= MAX_LENGTH or len(data["code2"]) >= MAX_LENGTH:
             return False
-        else :
+        else:
             return True
 
     dset = dset.filter(filter_fn, num_proc=CPU_COUNT)
@@ -67,8 +69,12 @@ def main():
     MODEL_CATEGORY = training_args.model_category
 
     tokenizer = AutoTokenizer.from_pretrained(model_args.PLM)
-    encoder = Encoder(tokenizer, model_category=MODEL_CATEGORY, max_input_length=data_args.max_length)
-    dset = dset.map(encoder, batched=True, num_proc=CPU_COUNT, remove_columns=dset['train'].column_names)
+    encoder = Encoder(
+        tokenizer, model_category=MODEL_CATEGORY, max_input_length=data_args.max_length
+    )
+    dset = dset.map(
+        encoder, batched=True, num_proc=CPU_COUNT, remove_columns=dset["train"].column_names
+    )
     print(dset)
 
     # -- Config & Model Class
@@ -76,59 +82,57 @@ def main():
     config.num_labels = 2
     config.tokenizer_cls_token_id = tokenizer.cls_token_id
     config.tokenizer_sep_token_id = tokenizer.sep_token_id
-    
+
     MODEL_NAME = training_args.model_name
-    if MODEL_NAME == 'base' :
+    if MODEL_NAME == "base":
         model_class = AutoModelForSequenceClassification
-    else :
-        model_category = importlib.import_module('models.' + MODEL_CATEGORY)
+    else:
+        model_category = importlib.import_module("models." + MODEL_CATEGORY)
         model_class = getattr(model_category, MODEL_NAME)
-   
+
     # -- Collator
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=data_args.max_length)
 
     if training_args.do_train:
         training_args.remove_unused_columns = False
         model = model_class.from_pretrained(model_args.PLM, config=config)
-         
+
         # -- Wandb
         WANDB_AUTH_KEY = os.getenv("WANDB_AUTH_KEY")
         wandb.login(key=WANDB_AUTH_KEY)
 
-        if training_args.max_steps == -1 :
+        if training_args.max_steps == -1:
             name = f"EP:{training_args.num_train_epochs}_"
-        else :
+        else:
             name = f"MS:{training_args.max_steps}_"
-            
+
         name += f"LR:{training_args.learning_rate}_BS:{training_args.per_device_train_batch_size}_WR:{training_args.warmup_ratio}_WD:{training_args.weight_decay}_"
         name += MODEL_NAME
-        
+
         wandb.init(
-            entity="poolc",
-            project=logging_args.project_name,
-            group=model_args.PLM,
-            name=name
+            entity="poolc", project=logging_args.project_name, group=model_args.PLM, name=name
         )
         wandb.config.update(training_args)
 
-        trainer = Trainer(                          # the instantiated 🤗 Transformers model to be trained
-            model=model,                            # model
-            args=training_args,                     # training arguments, defined above
-            train_dataset=dset['train'],            # training dataset
-            eval_dataset=dset['val'],               # evaluation dataset
-            data_collator=data_collator,            # collator
-            tokenizer=tokenizer,                    # tokenizer
-            compute_metrics=compute_metrics,        # define metrics function
+        trainer = Trainer(  # the instantiated 🤗 Transformers model to be trained
+            model=model,  # model
+            args=training_args,  # training arguments, defined above
+            train_dataset=dset["train"],  # training dataset
+            eval_dataset=dset["val"],  # evaluation dataset
+            data_collator=data_collator,  # collator
+            tokenizer=tokenizer,  # tokenizer
+            compute_metrics=compute_metrics,  # define metrics function
         )
 
         # -- Training
         trainer.train()
         trainer.evaluate()
         trainer.save_model(model_args.save_path)
-        wandb.finish()  
-        
+        wandb.finish()
+
+
 def seed_everything(seed):
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -138,5 +142,6 @@ def seed_everything(seed):
     np.random.default_rng(seed)
     random.seed(seed)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
