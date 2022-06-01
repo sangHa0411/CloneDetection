cbcb7f5b54a436225ff81b9932f6d0e4daf581e0
snoop2head <young_ahn@yonsei.ac.kr>
2022-06-01 15:39:13 +0900

    make trainer 5 fold training

--- train.py
+++ train.py
@@ -28,6 +28,7 @@ def main():
         (ModelArguments, DataTrainingArguments, MyTrainingArguments, LoggingArguments)
     )
     model_args, data_args, training_args, logging_args = parser.parse_args_into_dataclasses()
+    output_dir = training_args.output_dir
     seed_everything(training_args.seed)
 
     load_dotenv(dotenv_path=logging_args.dotenv_path)
@@ -35,115 +36,124 @@ def main():
 
     # -- Loading datasets
     # dset = load_dataset("PoolC/clone-det-base", use_auth_token=POOLC_AUTH_KEY)
-    dset = load_dataset("PoolC/clone-det-all", use_auth_token=True)
-    random_numbers_train = np.random.randint(
-        0, 6961241, int(1000000)
-    )  # can be set as a parameter if K-Fold is used
-    random_numbers_val = np.random.randint(
-        0, 6965880, int(100000)
-    )  # can be set as a parameter if K-Fold is used
-    dset["train"] = dset["train"].select(random_numbers_train)
-    dset["val"] = dset["val"].select(random_numbers_val)
-    print(dset)
+    # dset = load_dataset("PoolC/clone-det-all", use_auth_token=True)
+    for fold_index in range(0, 5):
+        dataset_name = f"snoop2head/{fold_index+1}-fold-clone-detection-600k-5fold"
+        dset = load_dataset(dataset_name, use_auth_token=True)
+        random_numbers_train = np.random.randint(
+            0, len(dset["train"]), int(179000 * 5)
+        )  # can be set as a parameter if K-Fold is used
+        random_numbers_val = np.random.randint(
+            0, len(dset["val"]), int(179000)
+        )  # can be set as a parameter if K-Fold is used
+        dset["train"] = dset["train"].select(random_numbers_train)
+        dset["val"] = dset["val"].select(random_numbers_val)
+        print(dset)
 
-    CPU_COUNT = multiprocessing.cpu_count() // 2
-    MODEL_NAME = training_args.model_name
-    MAX_LENGTH = 4000
+        CPU_COUNT = multiprocessing.cpu_count() // 2
+        MODEL_NAME = training_args.model_name
+        MAX_LENGTH = 4000
 
-    def filter_fn(data):
-        if len(data["code1"]) >= MAX_LENGTH or len(data["code2"]) >= MAX_LENGTH:
-            return False
-        else:
-            return True
+        def filter_fn(data):
+            if len(data["code1"]) >= MAX_LENGTH or len(data["code2"]) >= MAX_LENGTH:
+                return False
+            else:
+                return True
 
-    dset = dset.filter(filter_fn, num_proc=CPU_COUNT)
-    print(dset)
+        dset = dset.filter(filter_fn, num_proc=CPU_COUNT)
+        print(dset)
 
-    # -- Preprocessing datasets
-    if "bert" in MODEL_NAME.lower():
-        fn_preprocessor = FunctionPreprocessor()
-        dset = dset.map(fn_preprocessor, batched=True, num_proc=CPU_COUNT)
-
-        an_preprocessor = AnnotationPreprocessor()
-        dset = dset.map(an_preprocessor, batched=True, num_proc=CPU_COUNT)
-    elif "t5" in MODEL_NAME.lower() or "bart" in MODEL_NAME.lower():
-        preprocessor = BasePreprocessor()
-        dset = dset.map(preprocessor, batched=True, num_proc=CPU_COUNT)
-    # -- Tokenizing & Encoding
-    MODEL_CATEGORY = training_args.model_category
-
-    tokenizer = AutoTokenizer.from_pretrained(model_args.PLM)
-    encoder = Encoder(
-        tokenizer, model_category=MODEL_CATEGORY, max_input_length=data_args.max_length
-    )
-    dset = dset.map(
-        encoder, batched=True, num_proc=CPU_COUNT, remove_columns=dset["train"].column_names
-    )
-    print(dset)
-
-    # -- Config & Model Class
-    config = AutoConfig.from_pretrained(model_args.PLM)
-    config.num_labels = 2
-    config.tokenizer_cls_token_id = tokenizer.cls_token_id
-    config.tokenizer_sep_token_id = tokenizer.sep_token_id
-
-    if MODEL_NAME == "base":
-        model_class = AutoModelForSequenceClassification
-    else:
-        model_category = importlib.import_module("models." + MODEL_CATEGORY)
-        model_class = getattr(model_category, MODEL_NAME)
-
-    # -- Collator
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=data_args.max_length)
-
-    if training_args.do_train:
-        training_args.remove_unused_columns = False
-        model = model_class.from_pretrained(model_args.PLM, config=config)
-
-        # -- Wandb
-        WANDB_AUTH_KEY = os.getenv("WANDB_AUTH_KEY")
-        wandb.login(key=WANDB_AUTH_KEY)
-
-        if training_args.max_steps == -1:
-            name = f"EP:{training_args.num_train_epochs}_"
-        else:
-            name = f"MS:{training_args.max_steps}_"
-
-        name += f"LR:{training_args.learning_rate}_BS:{training_args.per_device_train_batch_size}_WR:{training_args.warmup_ratio}_WD:{training_args.weight_decay}_"
-        name += MODEL_NAME
-
-        wandb.init(
-            entity="poolc", project=logging_args.project_name, group=model_args.PLM, name=name
-        )
-        wandb.config.update(training_args)
+        # -- Preprocessing datasets
         if "bert" in MODEL_NAME.lower():
-            print("using RDrop applied trainer")
-            trainer = ImprovedRDropTrainer(  # the instantiated 🤗 Transformers model to be trained
-                model=model,  # model
-                args=training_args,  # training arguments, defined above
-                train_dataset=dset["train"],  # training dataset
-                eval_dataset=dset["val"],  # evaluation dataset
-                data_collator=data_collator,  # collator
-                tokenizer=tokenizer,  # tokenizer
-                compute_metrics=compute_metrics,  # define metrics function
-            )
-        elif "t5" in MODEL_NAME.lower() or "bart" in MODEL_NAME.lower():
-            print("using huggingface trainer")
-            trainer = Trainer(  # the instantiated 🤗 Transformers model to be trained
-                model=model,  # model
-                args=training_args,  # training arguments, defined above
-                train_dataset=dset["train"],  # training dataset
-                eval_dataset=dset["val"],  # evaluation dataset
-                data_collator=data_collator,  # collator
-                tokenizer=tokenizer,  # tokenizer
-                compute_metrics=compute_metrics,  # define metrics function
-            )
+            fn_preprocessor = FunctionPreprocessor()
+            dset = dset.map(fn_preprocessor, batched=True, num_proc=CPU_COUNT)
 
-        # -- Training
-        trainer.train()
-        trainer.evaluate()
-        trainer.save_model(model_args.save_path)
-        wandb.finish()
+            an_preprocessor = AnnotationPreprocessor()
+            dset = dset.map(an_preprocessor, batched=True, num_proc=CPU_COUNT)
+        elif "t5" in MODEL_NAME.lower() or "bart" in MODEL_NAME.lower():
+            preprocessor = BasePreprocessor()
+            dset = dset.map(preprocessor, batched=True, num_proc=CPU_COUNT)
+        # -- Tokenizing & Encoding
+        MODEL_CATEGORY = training_args.model_category
+
+        tokenizer = AutoTokenizer.from_pretrained(model_args.PLM)
+        encoder = Encoder(
+            tokenizer, model_category=MODEL_CATEGORY, max_input_length=data_args.max_length
+        )
+        dset = dset.map(
+            encoder, batched=True, num_proc=CPU_COUNT, remove_columns=dset["train"].column_names
+        )
+        print(dset)
+
+        # -- Config & Model Class
+        config = AutoConfig.from_pretrained(model_args.PLM)
+        config.num_labels = 2
+        config.tokenizer_cls_token_id = tokenizer.cls_token_id
+        config.tokenizer_sep_token_id = tokenizer.sep_token_id
+
+        if MODEL_NAME == "base":
+            model_class = AutoModelForSequenceClassification
+        else:
+            model_category = importlib.import_module("models." + MODEL_CATEGORY)
+            model_class = getattr(model_category, MODEL_NAME)
+
+        # -- Collator
+        data_collator = DataCollatorWithPadding(
+            tokenizer=tokenizer, max_length=data_args.max_length
+        )
+
+        if training_args.do_train:
+            training_args.remove_unused_columns = False
+            model = model_class.from_pretrained(model_args.PLM, config=config)
+
+            # -- Wandb
+            WANDB_AUTH_KEY = os.getenv("WANDB_AUTH_KEY")
+            wandb.login(key=WANDB_AUTH_KEY)
+
+            if training_args.max_steps == -1:
+                name = f"EP:{training_args.num_train_epochs}_"
+            else:
+                name = f"MS:{training_args.max_steps}_"
+
+            name += f"LR:{training_args.learning_rate}_BS:{training_args.per_device_train_batch_size}_WR:{training_args.warmup_ratio}_WD:{training_args.weight_decay}_"
+            name += MODEL_NAME
+            name += f"{fold_index+1}_fold"
+            num_train = str(random_numbers_train)[:-4] + "k"
+            name += f"_{num_train}"
+            training_args.output_dir = os.path.join(output_dir, f"{fold_index+1}_fold")
+
+            wandb.init(
+                entity="poolc", project=logging_args.project_name, group=model_args.PLM, name=name
+            )
+            wandb.config.update(training_args)
+            if "bert" in MODEL_NAME.lower():
+                print("using RDrop applied trainer")
+                trainer = ImprovedRDropTrainer(  # the instantiated 🤗 Transformers model to be trained
+                    model=model,  # model
+                    args=training_args,  # training arguments, defined above
+                    train_dataset=dset["train"],  # training dataset
+                    eval_dataset=dset["val"],  # evaluation dataset
+                    data_collator=data_collator,  # collator
+                    tokenizer=tokenizer,  # tokenizer
+                    compute_metrics=compute_metrics,  # define metrics function
+                )
+            elif "t5" in MODEL_NAME.lower() or "bart" in MODEL_NAME.lower():
+                print("using huggingface trainer")
+                trainer = Trainer(  # the instantiated 🤗 Transformers model to be trained
+                    model=model,  # model
+                    args=training_args,  # training arguments, defined above
+                    train_dataset=dset["train"],  # training dataset
+                    eval_dataset=dset["val"],  # evaluation dataset
+                    data_collator=data_collator,  # collator
+                    tokenizer=tokenizer,  # tokenizer
+                    compute_metrics=compute_metrics,  # define metrics function
+                )
+
+            # -- Training
+            trainer.train()
+            trainer.evaluate()
+            trainer.save_model(model_args.save_path)
+            wandb.finish()
 
 
 def seed_everything(seed):
